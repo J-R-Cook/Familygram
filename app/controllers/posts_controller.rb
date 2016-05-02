f1961b253fd9ae19a1923664471385f48cbbbfbe
Jason Cook <06cobaltls@gmail.com>
2016-05-02 10:06:08 -0700

    Changed some css & added Edit and Delete buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
@@ -8,13 +10,48 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create(post_params)
+		if @post = Post.create(post_params)
+			flash[:success] = "Your post has been created!"
 			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			flash[:success] = "Post updated."
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Update failed. Please check the form."
+			render :edit
+		end
+	end
+
+	def destroy
+		if @post.destroy
+			flash[:success] = "Your post has been deleted!"
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your post could not be deleted!"
+			render :edit
+		end
 	end
 
 	private
 
 	def post_params
 		params.require(:post).permit(:image, :caption)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
 	end
 end
